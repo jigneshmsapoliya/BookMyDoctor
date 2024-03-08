8cc837de692ba213d8cc15d43b1bb6a5a67329a8
jigneshmsapoliya <jmsapoliya@gmail.com>
2024-03-07 23:06:57 -0500

    bugfix : toggle button is working

--- bookmydoctor-app/src/components/HomeComponent.jsx
+++ bookmydoctor-app/src/components/HomeComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import '../HomeComponent.css';
 import '../HomeComponent.js';
@@ -15,32 +15,19 @@ const GET_DOCTORS = gql`
   }
 `;
 
-const toggleAnswer = (id) => {
-  var answer = document.getElementById(`faq-answer-${id}`);
-  var toggleIcon = document.querySelector(`.faq-question[data-id="${id}"] .faq-toggle`);
-
-  if (answer && toggleIcon) {
-    if (answer.style.display === 'block') {
-      answer.style.display = 'none';
-      toggleIcon.textContent = '+';
-    } else {
-      answer.style.display = 'block';
-      toggleIcon.textContent = '-';
-    }
-  } else {
-    console.error(`Elements not found for id ${id}`);
-  }
-};
-
 const HomeComponent = () => {
+  const { loading, error, data } = useQuery(GET_DOCTORS);
+  const [faqAnswersVisible, setFaqAnswersVisible] = useState({});
 
-  const { loading, error, data } = useQuery(GET_DOCTORS)
+  const toggleAnswer = (id) => {
+    setFaqAnswersVisible((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
 
   return (
     <div>
-
-
-
       <div class="banner">
         <div class="banner-content">
           <h1>Get A Consultation From Your Best Doctor</h1>
@@ -50,21 +37,18 @@ const HomeComponent = () => {
         <img src="doctor_banner.jpg" alt="Right Banner Image" />
       </div>
 
-
-
       <div class="container mt-5">
         <div class="row">
-          {data?.doctors.map((doctor) => (
+          {data?.doctors.slice(0, 3).map((doctor) => (
             <div className="col-md-4" key={doctor._id}>
               <div className="card">
                 <img src={doctor.imgUrl} alt="Doctor 1" />
-                <h3>{doctor.firstName}{doctor.lastName}</h3>
+                <h3>{doctor.firstName} {doctor.lastName}</h3>
                 <p>Specialist in {doctor.specialization}</p>
                 <button className="card-btn">Know More</button>
               </div>
             </div>
           ))}
-
         </div>
 
         <div class="find-more-container">
@@ -72,40 +56,39 @@ const HomeComponent = () => {
         </div>
       </div>
 
-
       {/* FAQs Section */}
       <div class="container mt-5 faq-container">
         <h2>Frequently Asked Questions</h2>
-        <div class="faq-item">
+        <div class={`faq-item ${faqAnswersVisible[1] ? 'faq-open' : ''}`}>
           <div class="faq-question" onClick={() => toggleAnswer(1)}>
-            How do I book an appointment with a doctor on this website?
-            <span class="faq-toggle">+</span>
-          </div>
-          <div class="faq-answer" id="faq-answer-1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo vitae libero consectetur gravida. Suspendisse potenti.
-          </div>
-        </div>
-        <div class="faq-item">
-          <div class="faq-question" onClick={() => toggleAnswer(2)}>
             What information do I need to provide when booking an appointment?
-            <span class="faq-toggle">+</span>
           </div>
-          <div class="faq-answer" id="faq-answer-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo vitae libero consectetur gravida. Suspendisse potenti.
-          </div>
-        </div>
-        <div class="faq-item">
-          <div class="faq-question" onClick={() => toggleAnswer(3)}>
-            Is my personal information secure on this website?
-            <span class="faq-toggle">+</span>
-          </div>
-          <div class="faq-answer" id="faq-answer-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo vitae libero consectetur gravida. Suspendisse potenti.
+          <div class="faq-answer" style={{ display: faqAnswersVisible[1] ? 'block' : 'none' }}>
+            Briefly describe the reason for your visit. This helps the doctor's office prepare for your appointment and allocate the appropriate amount of time.
+            You may also be asked to provide your insurance information, such as your insurance company name, policy number, and member ID.
           </div>
         </div>
-        {/* Add more FAQ items as needed */}
-      </div>
 
+        <div class={`faq-item ${faqAnswersVisible[2] ? 'faq-open' : ''}`}>
+          <div class="faq-question" onClick={() => toggleAnswer(2)}>
+            Is my personal information secure on this website?
+          </div>
+          <div class="faq-answer" style={{ display: faqAnswersVisible[2] ? 'block' : 'none' }}>
+            Our website uses SSL technology to encrypt all communication between your web browser and our servers.
+            We store your personal information on secure servers with limited access. We also employ industry-standard practices to protect your data from unauthorized access, modification, or disclosure.
+            We only collect the information necessary to provide you with the services you request. We will never share your personal information with third parties without your consent, except as required by law.
+          </div>
+        </div>
+
+        <div class={`faq-item ${faqAnswersVisible[3] ? 'faq-open' : ''}`}>
+          <div class="faq-question" onClick={() => toggleAnswer(3)}>
+            How do I book an appointment with a doctor on this website?
+          </div>
+          <div class="faq-answer" style={{ display: faqAnswersVisible[3] ? 'block' : 'none' }}>
+            Browse doctor profiles, learn about their expertise. Then, check the availability. After that you have to fill appointment details and click on book appointment button.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
